8299bf56513205d3cf39ce6ed465e1247937f5ac
muhand <muhandjumah@gmail.com>
2017-03-17 19:23:47 -0400

    Finished the example

--- WPFExample/WPFExample/MainWindow.xaml.cs
+++ WPFExample/WPFExample/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace WPFExample
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool open;
         public MainWindow()
         {
             InitializeComponent();
+            open = true;
+        }
+
+        private void MenuBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (open)
+            {
+                sideBarColumn.Width = new GridLength(0.05, GridUnitType.Star);
+                open = false;
+            }
+            else
+            {
+                sideBarColumn.Width = new GridLength(0.2, GridUnitType.Star);
+                open = true;
+            }
+        }
+
+        private void EventSetter_OnHandler(object sender, MouseEventArgs e)
+        {
+            Label t = (Label) sender;
+            t.Background = new SolidColorBrush(Colors.Aqua);
+            Cursor = Cursors.Hand;
+        }
+
+        private void EventSetters_OnHandler(object sender, MouseEventArgs e)
+        {
+            Label t = (Label)sender;
+            t.Background = new SolidColorBrush(Colors.Transparent);
+            Cursor = Cursors.Arrow;
         }
     }
 }
